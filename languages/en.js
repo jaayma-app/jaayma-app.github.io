1637c8a46bf7098857c464f6f91da0bea6823053
jaayma <jaayma.application@gmail.com>
2025-01-19 15:40:43 +0000

    Create en.js

---
+++ languages/en.js
@@ -0,0 +1,83 @@
+const en = {
+   "title": "Jaayma",
+   "home": "Home",
+   "about-us": "About Us",
+   "services": "Services",
+   "app-features": "App Features",
+   "join": "Join Us",
+   "home-top": "Be Among the First!",
+   "home-title": "Join Our Marketplace Revolution and",
+   "home-multi-1": "Buy",
+   "home-multi-2": "Sell",
+   "home-multi-3": "Deliver",
+   "home-multi-4": "Share",
+   "home-description": "Get exclusive early access and shape the future of Senegalese commerce with this innovative app, Jaayma.",
+   "home-button": "Get Early Access",
+   "about-title": "About ",
+   "about-subtitle": "Your All-in-One Marketplace",
+   "about-paragraph-1": "Jaayma is transforming Senegalese commerce into a seamless digital experience.",
+
+   "about-paragraph-2": "We connect local buyers with trusted sellers through reliable delivery partners across the country.",
+
+   "about-paragraph-3": "With integrated payment solutions including , ,  and  so that every transaction is secure and hassle-free.",
+   "groupe-1": "With integrated payment solutions including",
+   "groupe-2": "and",
+   "groupe-3": "so that every transaction is secure and hassle-free.",
+   "orange":"Orange Money",
+   "wave":"Wave",
+   "visa":"VISA",
+   "master-card":"MasterCard",
+
+   "about-paragraph-4": "Our real-time tracking system lets you monitor your deliveries every step of the way, from order to arrival.",
+
+   "service-title-1": "Our ",
+   "service-title-2": "Services",
+   "service-1": "Secure Transfers",
+   "service-1-description": "Enjoy peace of mind with Orange Money and Wave payment systems. We offer app balances for your in app purchases.",
+   "service-1-description-reveal": "Our robust security measures and user-friendly interface work together to provide you with a reliable, worry-free payment experience, so you can focus on what matters most.",
+   "service-2": "Customer Service",
+
+   "service-2-description-1": "We are here to help.",
+   "service-2-description-2": "Our customer support team is available to assist with any questions or issues either by phone email or live chat.",
+
+   "service-2-description-reveal": "Our goal is to ensure your experience is seamless and your concerns are addressed quickly. Don’t hesitate to reach out—we’re committed to making things easy for you!",
+   "service-3": "User Verification",
+   "service-3-description": "We verify users to ensure a safe and secure experience. This helps prevent scams and protects our community.",
+   "service-3-description-reveal": "This extra layer of security fosters a reliable environment where you can interact with confidence.",
+   "service-button":"Read More",
+   "service-button-less": "Read Less",
+
+   "features-title-1": "App ",
+   "features-title-2": "Features",
+
+   "feature-1": "Real-Time Communication",
+   "feature-1-description": "Communicate in real-time with your seller and deliverer through in app messages and calls.",
+
+   "feature-2": "Order Traking",
+   "feature-2-description": "Stay updated with your package location at all times.",
+
+   "feature-3": "Social-media Monitization",
+   "feature-3-description": "With your buyer account, laverage your social-media presence and get paid 0.5% of the price for each item you share per sale.",
+
+   "feature-4": "Inventory Management",
+   "feature-4-description": "Manage and keep track of your inventory.",
+
+   "feature-5": "Clear Guidance",
+   "feature-5-description": "We assist our deliverers with clear voice guidance to their chosen destination.",
+
+   "feature-6": "In person Payments",
+   "feature-6-description": "Sellers get paid by deliverers to guaranty product safety. <br> Clients are only required to pay once the order arrives.",
+
+   "get":"Get ",
+   "early-access":"Early Access",
+   "phone-placeholder": "Phone Number",
+   "email-placeholder": "Email",
+   "name-placeholder": "Name",
+   "select-user-type": "Select User Type",
+   "buyer": "Buyer",
+   "seller": "Seller",
+   "deliverer": "Deliverer",
+   "message-placeholder": "Message",
+   "join-now":"Join Now",
+   "copyright":"Copyright © 2025 by Saydi Hassane Thiam | All Rights Reserved.",
+};
