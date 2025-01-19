9550c3a71602d0788d2c2e6794a423afb35747e2
jaayma <jaayma.application@gmail.com>
2025-01-19 15:41:06 +0000

    Add files via upload

---
+++ languages/fr.js
@@ -0,0 +1,83 @@
+const fr = {
+    "title": "Jaayma",
+    "home": "Accueil",
+    "about-us": "À Propos",
+    "services": "Services",
+    "app-features": "Fonctionnalités",
+    "join": "Rejoignez-nous",
+    "home-top": "Sois Parmi les Premiers!",
+    "home-title": "Rejoin Notre Révolution du Marché et",
+    "home-multi-1": "Achète",
+    "home-multi-2": "Vends",
+    "home-multi-3": "Livre",
+    "home-multi-4": "Partage",
+    "home-description": "Obtenez un accès anticipé exclusif et façonnez l'avenir du commerce sénégalais avec la nouvelle application, Jaayma.",
+    "home-button": "Obtenir un Accès",
+    "about-title": "À Propos de ",
+    "about-subtitle": "Votre Marché Tout-en-Un",
+
+    "about-paragraph-1": "Jaayma transforme le commerce sénégalais en une expérience numérique fluide.",
+
+    "about-paragraph-2": "Nous connectons les acheteurs locaux avec des vendeurs de confiance à travers des partenaires de livraison fiables dans tout le pays.",
+
+    "groupe-1": "Avec des solutions de paiement intégrées incluant",
+    "groupe-2": "et",
+    "groupe-3": "pour que chaque transaction soit sécurisée et sans tracas.",
+    "orange":"Orange Money",
+    "wave":"Wave",
+    "visa":"VISA",
+    "master-card":"MasterCard",
+
+    "about-paragraph-4": "Notre système de suivi en temps réel vous permet de suivre vos livraisons à chaque étape, de la commande à l'arrivée.",
+
+    "service-title-1": "Nos ",
+    "service-title-2": "Services",
+    "service-1": "Transferts Sécurisés",
+    "service-1-description": "Profitez d'une tranquillité d'esprit avec les systèmes de paiement Orange Money et Wave. Nous proposons des soldes d'application pour vos achats in-app.",
+    "service-1-description-reveal": "Nos mesures de sécurité robustes et notre interface conviviale fonctionnent ensemble pour vous offrir une expérience de paiement fiable et sans souci, afin que vous puissiez vous concentrer sur l'essentiel.",
+    
+    "service-2": "Service Client",
+
+    "service-2-description-1": "Nous sommes là pour vous aider.",
+    "service-2-description-2": "Notre équipe de support client est disponible pour répondre à vos questions ou problèmes par téléphone, email ou chat en direct.",
+
+    "service-2-description-reveal": "Notre objectif est de garantir que votre expérience soit fluide et que vos préoccupations soient traitées rapidement. N'hésitez pas à nous contacter - nous nous engageons à vous faciliter la vie !",
+    "service-3": "Vérification des Utilisateurs",
+    "service-3-description": "Nous vérifions les utilisateurs pour garantir une expérience sûre et sécurisée. Cela aide à prévenir les arnaques et protège notre communauté.",
+    "service-3-description-reveal": "Cette couche de sécurité supplémentaire favorise un environnement fiable où vous pouvez interagir en toute confiance.",
+    "service-button": "En Savoir Plus",
+    "service-button-less": "Lire Moins",
+
+    "features-title-1": "Nos ",
+    "features-title-2": "Fonctionnalités",
+    "feature-1": "Communication en Temps Réel",
+    "feature-1-description": "Communiquez en temps réel avec votre vendeur et livreur grâce aux messages et appels dans l'application.",
+    
+    "feature-2": "Suivi de Commande",
+    "feature-2-description": "Restez informé de la localisation de votre colis à tout moment.",
+
+    "feature-3": "Monétisation des Réseaux Sociaux",
+    "feature-3-description": "Avec votre compte acheteur, tirez profit de votre présence sur les réseaux sociaux et recevez 0,5% du prix pour chaque article que vous partagez par vente.",
+
+    "feature-4": "Gestion des Stocks",
+    "feature-4-description": "Gérez et suivez votre inventaire.",
+
+    "feature-5": "Guidage Précis",
+    "feature-5-description": "Nous assistons nos livreurs avec un guidage vocal précis vers leur destination choisie.",
+
+    "feature-6": "Paiements en Personne",
+    "feature-6-description": "Les vendeurs sont payés par les livreurs pour garantir la sécurité des produits. <br> Les clients ne paient qu'à l'arrivée de la commande.",
+
+    "get": "Obtenir l'",
+    "early-access": "Accès anticipé",
+    "phone-placeholder": "Téléphone",
+    "email-placeholder": "Email",
+    "name-placeholder": "Nom",
+    "select-user-type": "Sélectionnez le type d'utilisateur",  
+    "buyer": "Acheteur",  
+    "seller": "Vendeur",  
+    "deliverer": "Livreur",  
+    "message-placeholder": "Message",
+    "join-now": "Rejoindre maintenant",
+    "copyright": "Droits d'auteur © 2025 par Saydi Hassane Thiam | Tous droits réservés.",
+ };
---
+++ languages/wo.js
@@ -0,0 +1,83 @@
+const wo = {
+    "title": "Jaayma",
+    "home": "Dalal",
+    "about-us": "Xamm ñu",
+    "services": "Services yi",
+    "app-features": "Mbir yi",  
+    "join": "Bokkal ci ñun",
+    "home-top": "Ñëkkënte si!",
+    "home-title": "Bokkal ci sunu révolution lëjum bi te tambalel",
+    "home-multi-1": "Jënd",
+    "home-multi-2": "Jaay", 
+    "home-multi-3": "Yóbbu",
+    "home-multi-4": "Seddoo",
+    "home-description": "Amal accès bu jëkk te exclusif te bokkalé ci réforme bi ñuy defar ci commerce Sénégalais bi ak aplikasiong bu bess bi di Jaayma.",
+    "home-button": "Bokk ci ñi Ñëkk",
+    "about-title": "Xamm ",
+    "about-subtitle": "Sa Lëjum bu Mat Sëkk",
+
+    "about-paragraph-1": "Jaayma dafay soppi commerce Sénégalais bi ci expérience numérique bu set wecc.",
+
+    "about-paragraph-2": "Danuy jokkale jëndkat yi ak jaaykat yu wóor yi jaare ci partenaires yuy yóbbu lu wóor ci réew mi yëpp.",
+
+    "groupe-1": "Ak solutions paiement yu bokkoo ni",
+    "groupe-2": "ak",
+    "groupe-3": "ngir bépp transaction am kaarange te yomb.",
+    "orange":"Orange Money",
+    "wave":"Wave",
+    "visa":"VISA",
+    "master-card":"MasterCard",
+
+    "about-paragraph-4": "Sunu système suivi en temps réel dina la may nga xamm sa yóbbu yi ci bépp étape, li dale ci commande bi ba arrivée bi.",
+
+    "service-title-1": "Sunuy ",
+    "service-title-2": "Services yi",
+    "service-1": "Yobal yu Wóor",
+    "service-1-description": "Am nga jàmm ak xel bu dal ak Orange Money ak Wave. Yangi lay joxe ngir nga mana jënd ci biir aplikasiong bi.",
+    "service-1-description-reveal": "Sunu kaarange yi ak sunu way-way bu yomb dañuy doxal ci benn yoon ngir jox la paiement bu wóor, ba nga mana koncentre ci li gën a am solo.",
+    
+    "service-2": "Service Client",
+
+    "service-2-description-1": "Ñu ngi fi ngir dimbali la.",
+    "service-2-description-2": "Sunu équipe bu service client bi dafa nekk fi ngir dimbali la ci bépp laaj walla jafe-jafe ci téléphone, email walla live chat.",
+
+    "service-2-description-reveal": "Sunu objectif mooy def ba nga am expérience bu set te sunu réponse yi gaaw. Bul hésité jokkoo ak ñun - dañu determined ngir defal la lu yomb!",
+    "service-3": "Wérification yu Utilisateur yi",
+    "service-3-description": "Danuy vérifier utilisateur yi ngir am sécurité. Lii dafay eviter ay naxante te moom sunu communauté bi.",
+    "service-3-description-reveal": "Lii mooy yokk sécurité bi ngir am environment bu wóor foo mëna interact ak confiance.",
+    "service-button": "Gëna Xam",
+    'service-button-less': 'Wañi Jàng',
+
+    "features-title-1": "Mbir ",
+    "features-title-2": "Yi ñu am",
+    "feature-1": "Jokkoo gu Gaaw",
+    "feature-1-description": "Wax ak sa jaaykat ak sa yóbbkat ci biir aplikasiong bi, ci mesaas walla wax ju dëpp.",
+
+    "feature-2": "Toppu sa Komaand",
+    "feature-2-description": "Xam fu sa yëf yi nekk ci bépp waxtu.",
+
+    "feature-3": "Jëfandikoo Réseaux Sociaux yi",
+    "feature-3-description": "Ak sa kont jëndkat, jëfandikool sa wër gi ci réseaux sociaux yi te am 0,5% ci njëg wi boo ko seddee ci yëf yi nga jaay.",
+
+    "feature-4": "Saytu sa Yëf yi",
+    "feature-4-description": "Saytu ak toppu sa yëf yi nga am.",
+
+    "feature-5": "Wommat bu Leer",
+    "feature-5-description": "Danuy dimmali sunuy yóbbkat yi ak wommat bu leer ngir ñu jot fu ñu jëm.",
+
+    "feature-6": "Fey ci bu Yegge",
+    "feature-6-description": "Yóbbkat yi dañuy fey jaaykat yi ngir wóor ne yëf yi dañu wóor. <br> Jëndkat yi waruñu fey lu moy bu komaand bi agsee.",
+
+    "get": "Jot",
+    "early-access": "Jot bu Ñëkk",
+    "phone-placeholder": "Téléfoon",  
+    "email-placeholder": "Imeel",  
+    "name-placeholder": "Tur",
+    "select-user-type": "Tànn melokaanu jëfandikukat",  
+    "buyer": "Jëndkat",  
+    "seller": "Jaaykat",  
+    "deliverer": "Yónnee-kat",    
+    "message-placeholder": "Xëtal",
+    "join-now": "Bokk leegi",
+    "copyright": "Sañ-sañu bind © 2025 ci Saydi Hassane Thiam | Sañ-sañ yépp ñoo ko moom.",
+ };
