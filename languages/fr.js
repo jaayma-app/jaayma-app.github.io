85e21f6b7ca8d43cdbf7cec5e7fc753aadcf90d2
jaayma <jaayma.application@gmail.com>
2025-04-30 17:06:18 +0000

    Update fr.js

--- languages/fr.js
+++ languages/fr.js
@@ -7,14 +7,15 @@ const fr = {
     "join": "Rejoignez-nous",
     "payment": "Paiements",
     "network-error": "Erreur de connection",
+    "appstoreMessage": "Désolé, Jaayma n'est pas encore disponible sur iPhone. Veuillez utiliser un appareil Android pour continuer.",
     "thank-you-message": "Merci pour votre candidature ! Notre service client vous contactera bientôt.",
     "home-top": "Sois Parmi les Premiers!",
-    "home-title": "Rejoins Notre Révolution du Marché et",
+    "home-title": "Rejoin Notre Révolution du Marché et",
     "home-multi-1": "Achète",
     "home-multi-2": "Vends",
     "home-multi-3": "Livre",
     "home-multi-4": "Partage",
-    "home-description": "Obtiens un accès anticipé exclusif et façonne l'avenir du commerce sénégalais avec la nouvelle application Jaayma.",
+    "home-description": "Obtenez un accès anticipé exclusif et façonnez l'avenir du commerce sénégalais avec la nouvelle application, Jaayma.",
     "home-button": "Obtenir un Accès",
     "about-title": "À Propos de ",
     "about-subtitle": "Votre Marché Tout-en-Un",
@@ -23,56 +24,58 @@ const fr = {
 
     "about-paragraph-2": "Nous connectons les acheteurs locaux avec des vendeurs de confiance à travers des partenaires de livraison fiables dans tout le pays.",
 
-    "groupe-1": "Avec des solutions de paiement incluant notamment",
+    "groupe-1": "Avec des solutions de paiement intégrées incluant",
     "groupe-2": "et",
-    "groupe-3": "pour des transactions sécurisées.",
+    "groupe-3": "pour que chaque transaction soit sécurisée et sans tracas.",
     "orange":"Orange Money",
     "wave":"Wave",
     "visa":"VISA",
     "master-card":"MasterCard",
 
-    "about-paragraph-4": "Notre système garanti aux utilisateurs un suivi en temps réel des livraisons à chaque étape, de la commande à la destination.",
+    "about-paragraph-4": "Notre système de suivi en temps réel vous permet de suivre vos livraisons à chaque étape, de la commande à l'arrivée.",
 
     "service-title-1": "Nos ",
     "service-title-2": "Services",
-    
     "service-1": "Transferts Sécurisés",
-    "service-1-description": "Profitez d'une tranquillité d'esprit avec les systèmes de paiement Orange Money et Wave.",
-    "service-1-description-reveal": "Nous vous permettons d'effectuer vos transferts à travers les systèmes de paiement mobiles qui vous sont familiers, afin de vous offrir une expérience de paiement fiable et sans souci, pour que vous puissiez vous concentrer sur l'essentiel.",
+    "service-1-description": "Profitez d'une tranquillité d'esprit avec les systèmes de paiement Orange Money et Wave. Nous proposons des soldes d'application pour vos achats in-app.",
+    "service-1-description-reveal": "Nos mesures de sécurité robustes et notre interface conviviale fonctionnent ensemble pour vous offrir une expérience de paiement fiable et sans souci, afin que vous puissiez vous concentrer sur l'essentiel.",
     
     "service-2": "Service Client",
+
     "service-2-description-1": "Nous sommes là pour vous aider.",
-    "service-2-description-2": "Notre équipe de support client est disponible pour répondre à vos questions ou problèmes par téléphone, email ou par WhatsApp.",
+    "service-2-description-2": "Notre équipe de support client est disponible pour répondre à vos questions ou problèmes par téléphone, email ou chat en direct.",
+
     "service-2-description-reveal": "Notre objectif est de garantir que votre expérience soit fluide et que vos préoccupations soient traitées rapidement. N'hésitez pas à nous contacter - nous nous engageons à vous faciliter la vie !",
-    
     "service-3": "Vérification des Utilisateurs",
-    "service-3-description": "Nous collectons les renseignements de nos utilisateurs pour garantir une expérience sécurisée à tous.",
-    "service-3-description-reveal": "Ces dispositions sécuritaires supplémentaires favorisent un environnement fiable où vous pouvez interagir en toute confiance.",
+    "service-3-description": "Nous vérifions les utilisateurs pour garantir une expérience sûre et sécurisée. Cela aide à prévenir les arnaques et protège notre communauté.",
+    "service-3-description-reveal": "Cette couche de sécurité supplémentaire favorise un environnement fiable où vous pouvez interagir en toute confiance.",
     "service-button": "En Savoir Plus",
     "service-button-less": "Lire Moins",
 
     "features-title-1": "Nos ",
     "features-title-2": "Fonctionnalités",
     "feature-1": "Communication en Temps Réel",
-    "feature-1-description": "Nous integrons des liens direct avec les messageries WhatsApp, vous permettant de communiquer en temps réel avec votre client, vendeur ou livreur.",
+    "feature-1-description": "Communiquez en temps réel avec votre vendeur et livreur grâce aux messages et appels dans l'application.",
     
-    "feature-2": "Suivi des Commandes",
-    "feature-2-description": "Restez informé de la localisation de votre colis tout en bénéficiant d'une livraison gratuite.",
+    "feature-2": "Suivi de Commande",
+    "feature-2-description": "Restez informé de la localisation de votre colis à tout moment.",
 
-    "feature-3": "Rentabilisez votre Présence en ligne",
-    "feature-3-description": "Avec votre compte acheteur, tirez profit de votre présence sur les réseaux sociaux et à chaque vente, recevez 0,5 % du prix de l'article que vous avez partagé.",
+    "feature-3": "Monétisation des Réseaux Sociaux",
+    "feature-3-description": "Avec votre compte acheteur, tirez profit de votre présence sur les réseaux sociaux et recevez 0,5% du prix pour chaque article que vous partagez par vente.",
 
     "feature-4": "Gestion des Stocks",
     "feature-4-description": "Gérez et suivez votre inventaire.",
 
     "feature-5": "Guidage Précis",
-    "feature-5-description": "Nous assistons nos livreurs avec un guidage vocal précis vers la destination choisie.",
+    "feature-5-description": "Nous assistons nos livreurs avec un guidage vocal précis vers leur destination choisie.",
 
     "feature-6": "Paiements en Personne",
-    "feature-6-description": "En tant qu'acheteur, vous ne payez qu'à l'arrivée de votre commande en utilisant votre méthode de paiement préférée. Les vendeurs reçoivent les informations (à verifier) de leurs livreurs pour garantir la sécurité des produits.",
+    "feature-6-description": "Les vendeurs sont payés par les livreurs pour garantir la sécurité des produits. <br> Les clients ne paient qu'à l'arrivée de la commande.",
 
     "get": "Obtenir l'",
     "early-access": "Accès anticipé",
+    "android": "Je dispose d'un Android",
+    "iphone": "Je dispose d'un Iphone",
     "phone-placeholder": "Téléphone",
     "email-placeholder": "Email",
     "name-placeholder": "Nom",
